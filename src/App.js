116fe25fe1c0e2898413031f27e606eb6ffa35d7
chistiakovnikita <nikita_chnv@mail.ru>
2023-09-23 18:03:18 +0300

    code-splitting

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
-import { useEffect } from 'react'
+import { lazy, Suspense, useEffect } from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { useDispatch } from 'react-redux'
 import Header from './components/Header'
-import HomePage from './components/pages/HomePage'
-import RegistrationPage from './components/pages/RegistrationPage'
-import LoginPage from './components/pages/LoginPage'
-import SinglePostPage from './components/pages/SinglePostPage'
+import Spinner from './components/Spinner'
 import { fetchAuthUser } from './redux/slices/authSlice'
 import './scss/App.scss'
+
+const HomePage = lazy(() => import('./components/pages/HomePage'))
+const RegistrationPage = lazy(() =>
+    import('./components/pages/RegistrationPage')
+)
+const LoginPage = lazy(() => import('./components/pages/LoginPage'))
+const SinglePostPage = lazy(() => import('./components/pages/SinglePostPage'))
 
 function App() {
     const dispatch = useDispatch()
@@ -20,15 +24,17 @@ function App() {
         <BrowserRouter>
             <div className="App">
                 <Header />
-                <Routes>
-                    <Route path="/" element={<HomePage />} />
-                    <Route
-                        path="/auth/registration"
-                        element={<RegistrationPage />}
-                    />
-                    <Route path="/auth/login" element={<LoginPage />} />
-                    <Route path="/posts/:id" element={<SinglePostPage />} />
-                </Routes>
+                <Suspense fallback={<Spinner />}>
+                    <Routes>
+                        <Route path="/" element={<HomePage />} />
+                        <Route
+                            path="/auth/registration"
+                            element={<RegistrationPage />}
+                        />
+                        <Route path="/auth/login" element={<LoginPage />} />
+                        <Route path="/posts/:id" element={<SinglePostPage />} />
+                    </Routes>
+                </Suspense>
             </div>
         </BrowserRouter>
     )
